b7e2e909381828d45fb3610a1898dee706a2970a
lzz.io <1219331697@qq.com>
2019-03-31 00:28:37 +0800

    p2p-ui init

---
+++ p2p-ui/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <template v-for="(item,index) in routes">
+                <router-link :to="item.path" v-bind:key="item.name+index">{{item.title}}</router-link>
+                <template v-if="index !== routes.length-1"> |</template>
+            </template>
+        </div>
+        <router-view />
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                routes: this.$router.options.routes
+            }
+        },
+        created: function () {
+            console.log(this.$router);
+        },
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ p2p-ui/src/router.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from './views/Index.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: Index,
+            title: '首页',
+        },
+        {
+            path: '/bid',
+            name: 'bid',
+            component: Index,
+            title: '我要投资',
+        },
+        {
+            path: '/',
+            name: 'user',
+            component: Index,
+            title: '用户中心',
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+            title: '关于我们',
+        }
+    ]
+})
